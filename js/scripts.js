4be39d1122b940ba0cf1f3f0c587a2b480219ae1
Dmitry <smirnov.dmitry.a@mail.ru>
2018-10-05 19:22:31 +0300

    new line

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,7 @@ feedback.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
   // name.focus();
+  
 });
 
 close.addEventListener("click", function(evt) {
